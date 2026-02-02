8a2bc09030ea6491585e43a23bbfe8d64f313ccb
Lordimass <lordimass@lordimass.net>
2026-02-02 09:13:18 +0000

    Require wand in hand to direct Clay Kweebec

--- src/main/java/com/clayfactoria/actions/ActionSetPath.java
+++ src/main/java/com/clayfactoria/actions/ActionSetPath.java
@@ -3,11 +3,15 @@ package com.clayfactoria.actions;
 import com.clayfactoria.actions.builders.BuilderActionSetPath;
 import com.clayfactoria.components.BrushComponent;
 import com.hypixel.hytale.builtin.path.path.TransientPathDefinition;
+import com.hypixel.hytale.component.ComponentType;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.math.vector.Vector3d;
 import com.hypixel.hytale.math.vector.Vector3f;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.inventory.Inventory;
+import com.hypixel.hytale.server.core.inventory.ItemStack;
 import com.hypixel.hytale.server.core.universe.world.path.IPath;
 import com.hypixel.hytale.server.core.universe.world.path.SimplePathWaypoint;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
@@ -20,50 +24,80 @@ import com.hypixel.hytale.server.npc.sensorinfo.InfoProvider;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-public class ActionSetPath  extends ActionBase {
-    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+/**
+ * Action triggered to finalise a created path and set it on the target entity.
+ */
+public class ActionSetPath extends ActionBase {
+  private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
 
-    @Nullable
-    protected final TransientPathDefinition pathDefinition;
+  @Nullable protected final TransientPathDefinition pathDefinition;
 
-    public ActionSetPath(@Nonnull BuilderActionSetPath builder, @Nonnull BuilderSupport support) {
-        super(builder);
-        this.pathDefinition = builder.getPath(support);
+  public ActionSetPath(@Nonnull BuilderActionSetPath builder, @Nonnull BuilderSupport support) {
+    super(builder);
+    this.pathDefinition = builder.getPath(support);
+  }
+
+  @Override
+  public boolean canExecute(
+      @Nonnull Ref<EntityStore> ref,
+      @Nonnull Role role,
+      InfoProvider sensorInfo,
+      double dt,
+      @Nonnull Store<EntityStore> store) {
+    return super.canExecute(ref, role, sensorInfo, dt, store);
+  }
+
+  public boolean execute(
+      @Nonnull Ref<EntityStore> ref,
+      @Nonnull Role role,
+      InfoProvider sensorInfo,
+      double dt,
+      @Nonnull Store<EntityStore> store) {
+    super.execute(ref, role, sensorInfo, dt, store);
+    Ref<EntityStore> playerRef = role.getStateSupport().getInteractionIterationTarget();
+    if (playerRef == null) {
+      return false;
     }
 
-    @Override
-    public boolean canExecute(@Nonnull Ref<EntityStore> ref, @Nonnull Role role, InfoProvider sensorInfo, double dt, @Nonnull Store<EntityStore> store) {
-        return super.canExecute(ref, role, sensorInfo, dt, store);
+    Player player = store.getComponent(playerRef, Player.getComponentType());
+    if (player == null) {
+      return false;
     }
 
-    public boolean execute(@Nonnull Ref<EntityStore> ref, @Nonnull Role role, InfoProvider sensorInfo, double dt, @Nonnull Store<EntityStore> store) {
-        super.execute(ref, role, sensorInfo, dt, store);
-        Ref<EntityStore> playerRef = role.getStateSupport().getInteractionIterationTarget();
-        if (playerRef == null) {
-            LOGGER.atSevere().log("Action Set Path: execute -> playerRef was null");
-            return false;
-        }
-
-        BrushComponent brushComponent = store.getComponent(playerRef, BrushComponent.getComponentType());
-        if (brushComponent == null) {
-            LOGGER.atSevere().log("Action Set Path: execute -> brushComponent was null");
-            return false;
-        }
-
-        NPCEntity npcComponent = store.getComponent(ref, NPCEntity.getComponentType());
-        if (npcComponent == null) {
-            LOGGER.atSevere().log("Action Set Path: execute -> npcComponent was null");
-            return false;
-        }
-
-        Vector3d pathStartPosition = brushComponent.getPathStartPosition();
-//        Vector3d pathEnd = brushComponent.getPathEndPosition();
-        Vector3f pathStartRotation = brushComponent.getPathStartRotation();
-
-        IPath<SimplePathWaypoint> path = this.pathDefinition.buildPath(pathStartPosition, pathStartRotation);
-        npcComponent.getPathManager().setTransientPath(path);
-
-        LOGGER.atInfo().log(String.format("Action Set Path: execute -> Successfully set path %s", brushComponent.getPathStart().toString()));
-        return true;
+    BrushComponent brushComponent =
+        store.getComponent(playerRef, BrushComponent.getComponentType());
+    if (brushComponent == null) {
+      LOGGER.atSevere().log("Action Set Path: execute -> brushComponent was null");
+      return false;
     }
+
+    ComponentType<EntityStore, NPCEntity> componentType = NPCEntity.getComponentType();
+    if (componentType == null) {
+      LOGGER.atSevere().log("Action Set Path: execute -> componentType was null");
+      return false;
+    }
+
+    NPCEntity npcComponent = store.getComponent(ref, componentType);
+    if (npcComponent == null) {
+      LOGGER.atSevere().log("Action Set Path: execute -> npcComponent was null");
+      return false;
+    }
+
+    Vector3d pathStartPosition = brushComponent.getPathStartPosition();
+    Vector3f pathStartRotation = brushComponent.getPathStartRotation();
+
+    if (this.pathDefinition == null) {
+      return false;
+    }
+
+    IPath<SimplePathWaypoint> path =
+        this.pathDefinition.buildPath(pathStartPosition, pathStartRotation);
+    npcComponent.getPathManager().setTransientPath(path);
+
+    LOGGER.atInfo().log(
+        String.format(
+            "Action Set Path: execute -> Successfully set path %s",
+            brushComponent.getPathStart().toString()));
+    return true;
+  }
 }
--- src/main/java/com/clayfactoria/systems/TargetBlockEventSystem.java
+++ src/main/java/com/clayfactoria/systems/TargetBlockEventSystem.java
@@ -14,6 +14,8 @@ import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.asset.type.soundevent.config.SoundEvent;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.event.events.ecs.DamageBlockEvent;
+import com.hypixel.hytale.server.core.inventory.Inventory;
+import com.hypixel.hytale.server.core.inventory.ItemStack;
 import com.hypixel.hytale.server.core.modules.entity.component.HeadRotation;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.ParticleUtil;
@@ -27,143 +29,133 @@ import javax.annotation.Nonnull;
 import java.awt.*;
 
 public class TargetBlockEventSystem extends EntityEventSystem<EntityStore, DamageBlockEvent> {
-    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
-    private final ComponentType<EntityStore, BrushComponent> brushComponentType = BrushComponent.getComponentType();
-    @Nonnull
-    private final ComponentType<EntityStore, NPCEntity> npcEntityComponentType;
+  /** ID of the item to use as a wand for setting Automaton paths. */
+  private static final String WAND_ITEM_ID = "Ingredient_Stick";
 
-    public TargetBlockEventSystem(@Nonnull ComponentType<EntityStore, NPCEntity> npcEntityComponentType) {
-        super(DamageBlockEvent.class);
-        this.npcEntityComponentType = npcEntityComponentType;
+  private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+  private final ComponentType<EntityStore, BrushComponent> brushComponentType =
+      BrushComponent.getComponentType();
+  @Nonnull private final ComponentType<EntityStore, NPCEntity> npcEntityComponentType;
+
+  public TargetBlockEventSystem(
+      @Nonnull ComponentType<EntityStore, NPCEntity> npcEntityComponentType) {
+    super(DamageBlockEvent.class);
+    this.npcEntityComponentType = npcEntityComponentType;
+  }
+
+  @Override
+  public void handle(
+      int index,
+      @NonNull ArchetypeChunk<EntityStore> archetypeChunk,
+      @NonNull Store<EntityStore> store,
+      @NonNull CommandBuffer<EntityStore> commandBuffer,
+      @NonNull DamageBlockEvent damageBlockEvent) {
+
+    Ref<EntityStore> entityStoreRef = archetypeChunk.getReferenceTo(index);
+
+    Player player = store.getComponent(entityStoreRef, Player.getComponentType());
+    if (player == null) return;
+
+    Ref<EntityStore> playerRef = player.getReference();
+    if (playerRef == null) {
+      LOGGER.atSevere().log("Target Block Event System: playerRef was null");
+      return;
     }
 
-    @Override
-    public void handle(int index, @NonNull ArchetypeChunk<EntityStore> archetypeChunk, @NonNull Store<EntityStore> store,
-                       @NonNull CommandBuffer<EntityStore> commandBuffer, @NonNull DamageBlockEvent damageBlockEvent) {
-
-        Ref<EntityStore> entityStoreRef = archetypeChunk.getReferenceTo(index);
-
-        Player player = store.getComponent(entityStoreRef, Player.getComponentType());
-        if (player == null) return;
-
-        Ref<EntityStore> playerRef = player.getReference();
-        if (playerRef == null) {
-            LOGGER.atSevere().log("Target Block Event System: playerRef was null");
-            return;
-        }
-
-        BrushComponent brushComponent = store.getComponent(playerRef, this.brushComponentType);
-        if (brushComponent == null) {
-            LOGGER.atSevere().log("Target Block Event System: Brush Component on the player was null");
-            return;
-        }
-
-        damageBlockEvent.setDamage(0);
-
-//        UUID targetEntityId = brushComponent.getTargetEntityId();
-//        if (targetEntityId == null) {
-//            player.sendMessage(Message.raw("Target Entity Id was null").color(Color.RED));
-//            LOGGER.atWarning().log("Target Block Event System: Brush Component -> Target Entity Id was null");
-//            return;
-//        }
-
-//        World world = player.getWorld();
-//        if (world == null) {
-//            LOGGER.atSevere().log("Target Block Event System: world was null");
-//            return;
-//        }
-//
-//        Ref<EntityStore> entityRef = world.getEntityRef(targetEntityId);
-//        if (entityRef == null) {
-//            LOGGER.atSevere().log("Target Block Event System: entityRef was null");
-//            return;
-//        }
-
-        // TODO: May need to use entityRef instead of entityStoreRef
-        HeadRotation headRotationComponent = store.getComponent(entityStoreRef, HeadRotation.getComponentType());
-        if (headRotationComponent == null){
-            LOGGER.atSevere().log("Target Block Event System: headRotationComponent was null");
-            return;
-        }
-
-        Vector3i targetBlockLoc = damageBlockEvent.getTargetBlock();
-        Vector3f headRotation = headRotationComponent.getRotation();
-
-        PathWaypoint pathWaypoint = new PathWaypoint(targetBlockLoc.x, targetBlockLoc.y, targetBlockLoc.z, headRotation.x, headRotation.y, headRotation.z);
-
-        if (brushComponent.getPathStart() == null) {
-            brushComponent.setPathStart(pathWaypoint);
-        } else if (brushComponent.getPathEnd() == null) {
-            brushComponent.setPathEnd(pathWaypoint);
-        } else {
-            brushComponent.setPathStart(null);
-            brushComponent.setPathEnd(null);
-            player.sendMessage(Message.raw("Resetting path...").color(Color.YELLOW));
-
-            ParticleUtil.spawnParticleEffect(
-                    "Block_Break_Dust",
-                    new Vector3d(targetBlockLoc.x + 0.5, targetBlockLoc.y + 1, targetBlockLoc.z + 0.5),
-                    store
-            );
-
-            SoundUtil.playSoundEvent2d(
-                    SoundEvent.getAssetMap().getIndex("SFX_Drag_Items_Clay"),
-                    SoundCategory.SFX,
-                    commandBuffer
-            );
-            return;
-        }
-
-        ParticleUtil.spawnParticleEffect(
-                "Block_Hit_Dirt",
-                new Vector3d(targetBlockLoc.x + 0.5, targetBlockLoc.y + 1, targetBlockLoc.z + 0.5),
-                store
-        );
-
-        SoundUtil.playSoundEvent2d(
-                SoundEvent.getAssetMap().getIndex("SFX_Drop_Items_Clay"),
-                SoundCategory.SFX,
-                commandBuffer
-        );
-
-        String message = String.format("Set Path Block: (%d, %d, %d)", targetBlockLoc.x, targetBlockLoc.y, targetBlockLoc.z);
-        LOGGER.atInfo().log(message);
-        player.sendMessage(Message.raw(message).color(Color.GREEN));
-
-
-//
-//        Store<EntityStore> entityStore = entityRef.getStore();
-//        NPCEntity npcEntity = entityStore.getComponent(entityRef, this.npcEntityComponentType);
-//        if (npcEntity == null) {
-//            return;
-//        }
-//
-//        Role role = npcEntity.getRole();
-//        if (role == null){
-//            LOGGER.atSevere().log("Target Block Event System: Target NPC Entity -> Role was null");
-//            return;
-//        }
-//        role.setMarkedTarget("LockedTarget", entityRef);
-//        npcEntity.getPathManager().setTransientPath();
-
-//        TransformComponent entityTransformComp = store.getComponent(entityRef, TransformComponent.getComponentType());
-//        if (entityTransformComp == null){
-//            LOGGER.atSevere().log("Target Block Event System: entityTransformComp was null");
-//            return;
-//        }
-//
-//        HeadRotation headRotationComponent = store.getComponent(entityRef, HeadRotation.getComponentType());
-//        if (headRotationComponent == null){
-//            LOGGER.atSevere().log("Target Block Event System: headRotationComponent was null");
-//            return;
-//        }
-//
-//        IPath<SimplePathWaypoint> path = npcEntity.getPathManager().get
-
-        }
-
-    @Override
-    public Query<EntityStore> getQuery() {
-        return PlayerRef.getComponentType();
+    // Check that the player has the wand equipped
+    if (!isWandEquipped(player)) {
+      return;
     }
+
+    BrushComponent brushComponent = store.getComponent(playerRef, this.brushComponentType);
+    if (brushComponent == null) {
+      LOGGER.atSevere().log("Target Block Event System: Brush Component on the player was null");
+      return;
+    }
+
+    damageBlockEvent.setDamage(0);
+
+    // TODO: May need to use entityRef instead of entityStoreRef
+    HeadRotation headRotationComponent =
+        store.getComponent(entityStoreRef, HeadRotation.getComponentType());
+    if (headRotationComponent == null) {
+      LOGGER.atSevere().log("Target Block Event System: headRotationComponent was null");
+      return;
+    }
+
+    Vector3i targetBlockLoc = damageBlockEvent.getTargetBlock();
+    Vector3f headRotation = headRotationComponent.getRotation();
+
+    PathWaypoint pathWaypoint =
+        new PathWaypoint(
+            targetBlockLoc.x,
+            targetBlockLoc.y,
+            targetBlockLoc.z,
+            headRotation.x,
+            headRotation.y,
+            headRotation.z);
+
+    if (brushComponent.getPathStart() == null) {
+      brushComponent.setPathStart(pathWaypoint);
+    } else if (brushComponent.getPathEnd() == null) {
+      brushComponent.setPathEnd(pathWaypoint);
+    } else {
+      brushComponent.setPathStart(null);
+      brushComponent.setPathEnd(null);
+      player.sendMessage(Message.raw("Resetting path...").color(Color.YELLOW));
+
+      ParticleUtil.spawnParticleEffect(
+          "Block_Break_Dust",
+          new Vector3d(targetBlockLoc.x + 0.5, targetBlockLoc.y + 1, targetBlockLoc.z + 0.5),
+          store);
+
+      SoundUtil.playSoundEvent2d(
+          SoundEvent.getAssetMap().getIndex("SFX_Drag_Items_Clay"),
+          SoundCategory.SFX,
+          commandBuffer);
+      return;
+    }
+
+    ParticleUtil.spawnParticleEffect(
+        "Block_Hit_Dirt",
+        new Vector3d(targetBlockLoc.x + 0.5, targetBlockLoc.y + 1, targetBlockLoc.z + 0.5),
+        store);
+
+    SoundUtil.playSoundEvent2d(
+        SoundEvent.getAssetMap().getIndex("SFX_Drop_Items_Clay"), SoundCategory.SFX, commandBuffer);
+
+    String message =
+        String.format(
+            "Set Path Block: (%d, %d, %d)", targetBlockLoc.x, targetBlockLoc.y, targetBlockLoc.z);
+    LOGGER.atInfo().log(message);
+    player.sendMessage(Message.raw(message).color(Color.GREEN));
+  }
+
+  @Override
+  public Query<EntityStore> getQuery() {
+    return PlayerRef.getComponentType();
+  }
+
+  /**
+   * Checks if the specified player is holding the wand item.
+   * @param player The player to check the condition in relation to.
+   * @return <code>true</code> if the player is holding the wand, <code>false</code> otherwise.
+   */
+  private boolean isWandEquipped(Player player) {
+    // Get player inventory.
+    Inventory inventory = player.getInventory();
+    if (inventory == null) {
+      return false;
+    }
+
+    // Get item in active hotbar slot.
+    byte slot = inventory.getActiveHotbarSlot();
+    ItemStack itemStack = inventory.getHotbar().getItemStack(slot);
+    if (itemStack == null) {
+      return false;
+    }
+
+    // Check if held item is the wand.
+    return itemStack.getItemId().equals(WAND_ITEM_ID);
+  }
 }
