57d90065caed8ab2080152b9805b3c94ccb95b6f
Lordimass <lordimass@lordimass.net>
2026-04-01 20:09:10 +0100

    Review tweaks

--- src/main/java/com/clayfactoria/systems/TaskBoxSystem.java
+++ src/main/java/com/clayfactoria/systems/TaskBoxSystem.java
@@ -8,7 +8,6 @@ import com.hypixel.hytale.component.ArchetypeChunk;
 import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.ComponentType;
 import com.hypixel.hytale.component.Store;
-import com.hypixel.hytale.component.SystemGroup;
 import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.system.tick.DelayedEntitySystem;
 import com.hypixel.hytale.component.system.tick.EntityTickingSystem;
@@ -17,20 +16,17 @@ import com.hypixel.hytale.math.vector.Vector3d;
 import com.hypixel.hytale.math.vector.Vector3f;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.modules.debug.DebugUtils;
-import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
-import com.hypixel.hytale.server.core.modules.entity.damage.DamageModule;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import java.util.List;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 public class TaskBoxSystem extends DelayedEntitySystem<EntityStore> {
 
   public static final Vector3d BOX_PADDING = new Vector3d(0.05F, 0.05F, 0.05F);
   private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
   @Nonnull
-  private static final ComponentType<EntityStore, TransformComponent> TRANSFORM_COMPONENT_TYPE = TransformComponent.getComponentType();
+  private static final ComponentType<EntityStore, Player> PLAYER_COMPONENT_TYPE = Player.getComponentType();
 
   public TaskBoxSystem() {
     super(0.5F);
@@ -88,15 +84,9 @@ public class TaskBoxSystem extends DelayedEntitySystem<EntityStore> {
         DebugUtils.FLAG_NO_WIREFRAME);
   }
 
-  @Nullable
-  @Override
-  public SystemGroup<EntityStore> getGroup() {
-    return DamageModule.get().getGatherDamageGroup();
-  }
-
   @Override
   public Query<EntityStore> getQuery() {
-    return TRANSFORM_COMPONENT_TYPE;
+    return PLAYER_COMPONENT_TYPE;
   }
 
   @Override
@@ -110,23 +100,27 @@ public class TaskBoxSystem extends DelayedEntitySystem<EntityStore> {
     TaskBoxesComponent taskBoxesComponent = archetypeChunk.getComponent(index,
         debugBoxesComponentType);
     Player player = archetypeChunk.getComponent(index, Player.getComponentType());
-    if (taskBoxesComponent == null || player == null || !TargetBlockEventSystem.isWandEquipped(
-        player)) {
+    if (taskBoxesComponent == null
+        || taskBoxesComponent.boxes.isEmpty()
+        || player == null
+        || !TargetBlockEventSystem.isWandEquipped(player)
+    ) {
       return;
     }
     World world = commandBuffer.getExternalData().getWorld();
 
     List<TaskBoxComponent> boxes = taskBoxesComponent.boxes;
-    for (int i = 0; i < boxes.size(); i++) {
+    TaskBoxComponent prev = boxes.getFirst();
+    drawBox(prev, world);
+    for (int i = 1; i < boxes.size(); i++) {
       TaskBoxComponent box = boxes.get(i);
       drawBox(box, world);
 
-      if (i + 1 < boxes.size()) {
-        TaskBoxComponent nextBox = boxes.get(i + 1);
-        Vector3d boxMid = box.getBox().min.clone().add(box.getBox().max).scale(0.5);
-        Vector3d nextBoxMid = nextBox.getBox().min.clone().add(nextBox.getBox().max).scale(0.5);
-        drawArrow(boxMid, nextBoxMid, world, box.getColour());
-      }
+      Vector3d boxMid = box.getBox().min.clone().add(box.getBox().max).scale(0.5);
+      Vector3d prevBoxMid = prev.getBox().min.clone().add(prev.getBox().max).scale(0.5);
+      drawArrow(prevBoxMid, boxMid, world, box.getColour());
+
+      prev = box;
     }
   }
 
