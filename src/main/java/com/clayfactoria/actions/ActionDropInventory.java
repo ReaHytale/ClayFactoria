fad55d612fca5756f5bb79b2695cf5ea38f81e87
Lordimass <lordimass@lordimass.net>
2026-04-09 13:50:03 +0100

    #68 Harvest Instruction Block
    Almost certainly will need tweaks once the task is properly implemented, but it now exists and should do what it needs to for the most part.

--- src/main/java/com/clayfactoria/actions/ActionDropInventory.java
+++ src/main/java/com/clayfactoria/actions/ActionDropInventory.java
@@ -7,11 +7,14 @@ import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.math.vector.Vector3d;
 import com.hypixel.hytale.server.core.entity.ItemUtils;
-import com.hypixel.hytale.server.core.inventory.ItemStack;
+import com.hypixel.hytale.server.core.inventory.InventoryComponent;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import com.hypixel.hytale.server.npc.asset.builder.BuilderSupport;
 import com.hypixel.hytale.server.npc.entities.NPCEntity;
 import com.hypixel.hytale.server.npc.role.Role;
 import com.hypixel.hytale.server.npc.sensorinfo.InfoProvider;
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.List;
 import javax.annotation.Nonnull;
 import org.jetbrains.annotations.NotNull;
@@ -20,8 +23,12 @@ public class ActionDropInventory extends ActionBaseLogger {
 
   private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
 
-  public ActionDropInventory(@NotNull BuilderActionDropInventory builder) {
+  private final boolean dropHotbarItems;
+
+  public ActionDropInventory(@NotNull BuilderActionDropInventory builder,
+      BuilderSupport builderSupport) {
     super(builder);
+    dropHotbarItems = builder.getDropHotbarItems(builderSupport);
   }
 
   @Override
@@ -35,11 +42,30 @@ public class ActionDropInventory extends ActionBaseLogger {
   public boolean execute(@Nonnull Ref<EntityStore> ref, @Nonnull Role role, InfoProvider sensorInfo,
       double dt, @Nonnull Store<EntityStore> store) {
     NPCEntity npc = TaskHelper.getNPCEntity(ref);
-    List<ItemStack> items = npc.getInventory().getCombinedStorageFirst().dropAllItemStacks();
-    items.forEach(item -> {
-      ItemUtils.throwItem(ref, store, item, Vector3d.ZERO, 1.0F);
-    });
+    List<String> hotbarItems = getHotbarItems(npc.getRole());
 
+    InventoryComponent.getCombined(store, ref).forEach((_, item) -> {
+      if (dropHotbarItems
+          || hotbarItems != null && !hotbarItems.contains(item.getItemId())
+      ) {
+        ItemUtils.throwItem(ref, store, item, Vector3d.ZERO, 1.0F);
+      }
+    });
     return true;
+  }
+
+  private List<String> getHotbarItems(Role role) {
+    try {
+      Field hotbarItemsField = Role.class.getDeclaredField("hotbarItems");
+      hotbarItemsField.setAccessible(true);
+      String[] hotbarItemsArray = (String[]) hotbarItemsField.get(role);
+      if (hotbarItemsArray != null) {
+        return Arrays.asList(hotbarItemsArray);
+      }
+      return null;
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      LOGGER.atSevere().log(e.getMessage());
+      return null;
+    }
   }
 }
--- src/main/java/com/clayfactoria/actions/builders/BuilderActionDropInventory.java
+++ src/main/java/com/clayfactoria/actions/builders/BuilderActionDropInventory.java
@@ -1,14 +1,24 @@
 package com.clayfactoria.actions.builders;
 
 import com.clayfactoria.actions.ActionDropInventory;
+import com.google.gson.JsonElement;
+import com.hypixel.hytale.server.npc.asset.builder.Builder;
 import com.hypixel.hytale.server.npc.asset.builder.BuilderDescriptorState;
 import com.hypixel.hytale.server.npc.asset.builder.BuilderSupport;
+import com.hypixel.hytale.server.npc.asset.builder.holder.BooleanHolder;
 import com.hypixel.hytale.server.npc.corecomponents.builders.BuilderActionWithDelay;
 import com.hypixel.hytale.server.npc.instructions.Action;
 import javax.annotation.Nonnull;
 import org.jetbrains.annotations.Nullable;
 
 public class BuilderActionDropInventory extends BuilderActionWithDelay {
+
+  BooleanHolder dropHotbarItems = new BooleanHolder();
+
+  public boolean getDropHotbarItems(BuilderSupport builderSupport) {
+    return dropHotbarItems.get(builderSupport.getExecutionContext());
+  }
+
   @Nonnull
   @Override
   public String getShortDescription() {
@@ -23,11 +33,25 @@ public class BuilderActionDropInventory extends BuilderActionWithDelay {
 
   @Nonnull
   public Action build(@Nonnull BuilderSupport builderSupport) {
-    return new ActionDropInventory(this);
+    return new ActionDropInventory(this, builderSupport);
   }
 
   @Override
   public @Nullable BuilderDescriptorState getBuilderDescriptorState() {
     return BuilderDescriptorState.Stable;
+  }
+
+  @Nonnull
+  public Builder<Action> readConfig(@Nonnull JsonElement data) {
+    this.getBoolean(
+        data,
+        "DropHotbarItems",
+        this.dropHotbarItems,
+        true,
+        BuilderDescriptorState.Stable,
+        "Whether to drop the items in the NPC role's HotbarItems",
+        "Whether to drop the items which are specified in the NPC role's HotbarItems. Useful if you want to ensure that those items stay in the NPC inventory when others do not."
+    );
+    return this;
   }
 }
