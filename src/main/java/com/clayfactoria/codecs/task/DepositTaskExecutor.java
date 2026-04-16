a72e70c98527bad08a35411129d5fe4ff37e03aa
Lordimass <lordimass@lordimass.net>
2026-04-16 15:05:56 +0100

    Check that container isn't full before deposit
    Related to ticket #98, hence being on this branch. There was already a check that the container isn't full, but the actual issue was the it was only checking if it could deposit the currently held item, not all of the items that it wanted to deposit, so it would run the deposit task and only manage to deposit some of the items.
    
    Closes #108

--- src/main/java/com/clayfactoria/codecs/task/DepositTaskExecutor.java
+++ src/main/java/com/clayfactoria/codecs/task/DepositTaskExecutor.java
@@ -4,21 +4,19 @@ import com.clayfactoria.codecs.Job;
 import com.clayfactoria.components.JobComponent;
 import com.clayfactoria.utils.ContainerSlot;
 import com.clayfactoria.utils.TaskHelper;
-import com.hypixel.hytale.component.Component;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.inventory.ItemStack;
 import com.hypixel.hytale.server.core.inventory.container.ItemContainer;
-import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.hypixel.hytale.server.npc.entities.NPCEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import static com.clayfactoria.utils.TaskHelper.getHeldItemstack;
 import static com.clayfactoria.utils.TaskHelper.getNPCEntity;
 
 public class DepositTaskExecutor extends PointTaskExecutor {
@@ -28,30 +26,21 @@ public class DepositTaskExecutor extends PointTaskExecutor {
     @Override
     public boolean canPerformTask(Ref<EntityStore> ref) {
         Store<EntityStore> store = ref.getStore();
-        Component<ChunkStore> blockEntity = TaskHelper.getBlockEntity(ref);
-        if (blockEntity == null) {
-            return false;
-        }
+        NPCEntity npc = getNPCEntity(ref);
+        List<String> hotbarItems = TaskHelper.getHotbarItems(npc.getRole());
 
-        ItemStack heldItemStack = getHeldItemstack(store, ref);
-
-        ItemContainer inputContainer =
-            TaskHelper.getItemContainerFromComponent(blockEntity, ContainerSlot.Input);
-        Objects.requireNonNull(inputContainer, "Unexpected null input ItemContainer");
-
-        ItemContainer fuelContainer =
-            TaskHelper.getItemContainerFromComponent(blockEntity, ContainerSlot.Fuel);
-        Objects.requireNonNull(fuelContainer, "Unexpected null fuel ItemContainer");
+        JobComponent jobComponent = store.getComponent(ref, JobComponent.getComponentType());
+        assert jobComponent != null;
+        Job job = jobComponent.getCurrentJob();
+        assert job != null;
 
         // There must be space in the container for the item stack, and there must be space in hands
-        return heldItemStack == null
-            || fuelContainer.canAddItemStack(heldItemStack)
-            || inputContainer.canAddItemStack(heldItemStack);
+        return canDeposit(ContainerSlot.Input, npc, job, hotbarItems)
+            || canDeposit(ContainerSlot.Fuel, npc, job, hotbarItems);
     }
 
     @Override
     public boolean execute(Ref<EntityStore> entityRef) {
-
         NPCEntity npcEntity = getNPCEntity(entityRef);
         Store<EntityStore> store = entityRef.getStore();
         JobComponent jobComponent = Objects.requireNonNull(
@@ -85,5 +74,24 @@ public class DepositTaskExecutor extends PointTaskExecutor {
             }
         });
         return !fail.get();
+    }
+
+    private boolean canDeposit(ContainerSlot containerSlot, NPCEntity npcEntity, Job currentJob,
+                               List<String> hotbarItems) {
+        Store<EntityStore> store = Objects.requireNonNull(npcEntity.getReference()).getStore();
+        ItemContainer itemContainer = TaskHelper.getItemContainerAtPos(
+            Objects.requireNonNull(npcEntity.getWorld()),
+            currentJob.getLocation(),
+            containerSlot);
+        Objects.requireNonNull(itemContainer);
+
+        ItemContainer npcInventory = TaskHelper.getNPCInventory(npcEntity, store);
+        List<ItemStack> itemStacks = new ArrayList<>();
+        npcInventory.forEach((_, itemStack) -> {
+            if (!hotbarItems.contains(itemStack.getItemId())) {
+                itemStacks.add(itemStack);
+            }
+        });
+        return itemContainer.canAddItemStacks(itemStacks);
     }
 }
